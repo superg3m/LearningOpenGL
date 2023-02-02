c78dae0dcf7e7570c29190c294caa3c6049b29da
superg3m <superg3m@gmail.com>
2023-02-01 19:36:25 -0500

    Included GLM (OpenGL Mathmatics)

--- Source.cpp
+++ Source.cpp
@@ -2,6 +2,10 @@
 #include <glad/glad.h>
 #include <glfw/glfw3.h>
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 #include "./shaderClass.h"
 #include "./stb_image.h"
 
