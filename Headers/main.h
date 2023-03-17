4ee5e65134b16147857b212c5cbc76412eea6e0d
superg3m <superg3m@gmail.com>
2023-03-17 18:53:08 -0400

    GOOD WORK

--- Headers/main.h
+++ Headers/main.h
@@ -39,6 +39,10 @@ void configureBufferAttributes(const unsigned int &position, const unsigned  int
 std::pair<float, float> circle_points(const float &radius, const float& angle, const glm::vec2& origin);
 glm::mat4 transformMatrix(glm::mat4& matrix, float angle, glm::vec3 vector_translate, glm::vec3 vector_rotate, glm::vec3 vector_scale);
 glm::quat safeQuatLookAt(glm::vec3& direction, glm::vec3 const& up);
+
+glm::quat RotationBetweenVectors(glm::vec3 start, glm::vec3 dest);
+glm::quat LookAt(glm::vec3 direction, glm::vec3 desiredUp);
+
 // Constants
 const bool TRANSLATION_ROTATION = true;
 const bool ORBIT = false;
--- Source Files/Main.cpp
+++ Source Files/Main.cpp
@@ -132,7 +132,7 @@ int main() {
 	#pragma endregion
 
 	// *************** Model ***************
-	Model modelObject("../../3D/Neon.fbx");
+	Model modelObject("../3D/Neon.fbx");
 
 	bool drawCubes = false;
 	float cubeSize = 1.0f;
@@ -211,22 +211,23 @@ int main() {
 		}
 		
 		
-		
 		// Number of splines
-		int number_of_splines = points.size() - 3;
+		int number_of_splines = points.size() - 1;
 		meshAmount.clear();
 		for (int i = 0; i < number_of_splines; i++)
 		{
-			meshAmount.push_back(10 * (glm::length(points[i] - points[i + 1])) / modelObject.height);
+			meshAmount.push_back(3 * glm::length(points[i] - points[i + 1]) / (modelObject.height));
 		}
 		if (number_of_splines > 0 && !pause)
 		{
-			
+			glm::vec3 ghostStart = 2.0f * points[0] - points[1];
+			glm::vec3 ghostEnd = 2.0f * points.back() - points[points.size() - 2];
 			for (int i = 0; i < number_of_splines; i++)
 			{
 				for (int j = 0; j < meshAmount[i]; j++)
 				{
-					splinePoints.push_back(splineObject.CatmullRom(points[0], points[i + 1], points[i + 2], points[points.size() - 1], j / (float)meshAmount[i]));
+					splinePoints.push_back(splineObject.CatmullRom(ghostStart, points[i], points[i + 1], ghostEnd, j / (float)meshAmount[i]));
+					//splinePoints.push_back(splineObject.CatmullRom(points[0], points[i], points[i + 1], points[points.size() - 1], j / (float)meshAmount[i]));
 				}
 			}
 			for (unsigned int i = 0; i < splinePoints.size() - 1; i++)
@@ -334,14 +335,14 @@ int main() {
 			glm::mat4 model = glm::mat4(1.0f);
 			model = glm::translate(model, splinePoints[i]);
 			
-			glm::quat quat = safeQuatLookAt(distanceVec[i], glm::vec3(0, 1, 0));
+			glm::quat quat = LookAt(distanceVec[i], glm::vec3(0, 1, 0));
 			glm::mat4 RotationMatrix = glm::toMat4(quat);
 			model = model * RotationMatrix;
 			model = glm::scale(model, glm::vec3(0.5f));
 			cubeShader.setMat4("model", model);
 			//cubeShader.setVec3("lightColor", glm::vec3(0.25f));
-			//cubeShader.setFloat("material.type", 1);
-			//cubeShader.setVec4("uniColor", glm::vec4(0.2, 1.0, 0.2f, 1.0));
+			cubeShader.setFloat("material.type", 1);
+			cubeShader.setVec4("uniColor", glm::vec4(0.2, 1.0, 0.2f, 1.0));
 			modelObject.Draw(cubeShader);
 		}
 		
@@ -600,18 +601,81 @@ glm::mat4 transformMatrix(glm::mat4& matrix, float angle, glm::vec3 vector_trans
 	return matrix;
 }
 
+glm::quat RotationBetweenVectors(glm::vec3 start, glm::vec3 dest) {
+	start = normalize(start);
+	dest = normalize(dest);
+
+	float cosTheta = glm::dot(start, dest);
+	glm::vec3 rotationAxis;
+
+	if (cosTheta < -1 + 0.001f) {
+		// special case when vectors in opposite directions :
+		// there is no "ideal" rotation axis
+		// So guess one; any will do as long as it's perpendicular to start
+		// This implementation favors a rotation around the Up axis,
+		// since it's often what you want to do.
+		rotationAxis = glm::cross(glm::vec3(0.0f, 0.0f, 1.0f), start);
+		if (glm::length2(rotationAxis) < 0.01) // bad luck, they were parallel, try again!
+			rotationAxis = glm::cross(glm::vec3(1.0f, 0.0f, 0.0f), start);
+
+		rotationAxis = normalize(rotationAxis);
+		return glm::angleAxis(glm::radians(180.0f), rotationAxis);
+	}
+
+	// Implementation from Stan Melax's Game Programming Gems 1 article
+	rotationAxis = glm::cross(start, dest);
+
+	float s = sqrt((1 + cosTheta) * 2);
+	float invs = 1 / s;
+
+	return glm::quat(
+		s * 0.5f,
+		rotationAxis.x * invs,
+		rotationAxis.y * invs,
+		rotationAxis.z * invs
+	);
+
+
+}
+
+
+glm::quat LookAt(glm::vec3 direction, glm::vec3 desiredUp) {
+
+	if (glm::length(direction) < 0.0001f)
+	{
+		return glm::quat(1, 0, 0, 0);
+	}
+		
+
+	// Recompute desiredUp so that it's perpendicular to the direction
+	// You can skip that part if you really want to force desiredUp
+	glm::vec3 right = glm::cross(direction, desiredUp);
+	desiredUp = cross(right, direction);
+
+	// Find the rotation between the front of the object (that we assume towards +Z,
+	// but this depends on your model) and the desired direction
+	glm::quat rot1 = RotationBetweenVectors(glm::vec3(0.0f, 0.0f, 1.0f), direction);
+	// Because of the 1rst rotation, the up is probably completely screwed up. 
+	// Find the rotation between the "up" of the rotated object, and the desired up
+	glm::vec3 newUp = rot1 * glm::vec3(0.0f, 1.0f, 0.0f);
+	glm::quat rot2 = RotationBetweenVectors(newUp, desiredUp);
+
+	// Apply them
+	return rot2 * rot1; // remember, in reverse order.
+}
+
 glm::quat safeQuatLookAt(glm::vec3 &direction,glm::vec3 const& up)
 {
 	float directionLength = glm::length(direction);
 
 	// Check if the direction is valid; Also deals with NaN
 	if (!(directionLength > 0.0001))
+	{
 		return glm::quat(1, 0, 0, 0); // Just return identity
-
+	}
 	// Normalize direction
-	direction = glm::normalize(direction);
-
-	return glm::quatLookAt(direction, up);
+	glm::vec3 normDirection = glm::normalize(direction);
+	return glm::quatLookAt(normDirection, up);
 }
 
 #pragma endregion
